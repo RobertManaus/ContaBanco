3fe32c76140cdd0ed8110f4578aefa64e83867f0
Roberto Silva <robeertjr@gmail.com>
2023-07-01 10:11:15 -0400

    Desafio conta banco basico

--- src/Principal.java
+++ src/Principal.java
@@ -3,22 +3,22 @@ import java.util.Scanner;
 public class Principal {
     
     public static void main(String[] args) {
-
+        
         ContaTerminal contaTerminal = new ContaTerminal();
         Scanner lerValores = new Scanner(System.in);
 
-        System.out.println("Digite o número da sua Conta..: ");
+        System.out.print("Digite o número da sua Conta..: ");
         contaTerminal.numero = lerValores.nextInt();
 
-        System.out.println("Digite o numero da sua Agencia..: ");
+        System.out.print("Digite o numero da sua Agencia..: ");
         contaTerminal.agencia = lerValores.next();
 
-        System.out.println("Nome do Cliente..: ");
+        System.out.print("Nome do Cliente..: ");
         contaTerminal.nomeCliente = lerValores.next();
 
         System.out.print("Seja bem-vindo " + contaTerminal.nomeCliente
-                + "obrigado por criar uma conta em nosso banco, sua agencia eh "
-                + contaTerminal.agencia + ", conta " + numero);
+                + "\nObrigado por criar uma conta em nosso banco \nsua agencia e "
+                + contaTerminal.agencia + "\nconta " + contaTerminal.numero);
 
         
 
